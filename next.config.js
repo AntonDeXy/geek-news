6854e8c0d337fb4a8dbd19fcd941fec4a8608d3a
DeXy <podolyananton@gmail.com>
2019-09-25 02:35:39 +0300

    next images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  webpack(config, options) {
+const withImages = require('next-images')
+
+module.exports = withImages(withSass({
+  webpack: config => {
+    config.node = {
+      fs: 'empty'
+    }
+
     return config
   }
-})
+}))
