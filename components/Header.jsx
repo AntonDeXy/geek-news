7baaa765673685fb7fb14c0ffe4e456c832164b6
AntonDeXy <porolyananton@gmail.com>
2019-09-29 13:01:33 +0300

    upd

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,34 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
+import styled from 'styled-components'
 
-const Header = () => {
+const Header = (props) => {
+  const [color, setColor] = useState('#383838')
+
+  const HeaderSt = styled.header`
+    background-color: ${color || '#383838'};
+  `
+  const changeTheme = () => {
+    debugger
+    
+    if (props.theme == 'dark') {
+      props.setTheme('light')
+      setColor('red')
+      console.log('light')
+    } else {
+      props.setTheme('dark')
+      setColor('#383838')
+      console.log('dark')
+    }
+  }
+
+
+
   return (
-    <header>
-      <div className="wrapper">
-        <Link href="/">
-          <a  className="logo">
-            <h1 className="logo">Geek News</h1>
-          </a>
-        </Link>
-        <div className="links">
-          <Link href="#">
-            <a>
-              Новости
+    <>
+      <HeaderSt>
+        <div className="wrapper">
+          <Link href="/">
+            <a className="logo">
+              <h1 className="logo">Geek News</h1>
             </a>
           </Link>
-          <Link href="#">
-            <a>
-              Личный кабинет
+          <button onClick={() => { changeTheme() }}>Change theme</button>
+          <div className="links">
+            <Link href="#">
+              <a>
+                Новости
             </a>
-          </Link>
-          <Link href="#">
-            <a>
-              Вход
+            </Link>
+            <Link href="#">
+              <a>
+                Личный кабинет
             </a>
-          </Link>
+            </Link>
+            <Link href="#">
+              <a>
+                Вход
+            </a>
+            </Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </HeaderSt>
+      <style jsx>{`
+        header {
+          backgroundColor: #383838;
+        }
+      `}</style>
+    </>
   );
 };
 
