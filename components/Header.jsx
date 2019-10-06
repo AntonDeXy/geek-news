93c934e5b222c85528fe0e1760783c8fe798a209
AntonDeXy <porolyananton@gmail.com>
2019-10-06 22:22:04 +0300

    header to slyted-components + fix bug with reload & styled components

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,31 +1,60 @@
-import React, { useState } from "react";
-import Link from "next/link";
+import React, { useState } from "react"
+import Link from "next/link"
+import styled from 'styled-components'
 
+const HeaderSt = styled.header`
+  display: grid;
+  color: white;
+  background-color: #383838;
+  width: 100%;
+  height: 10vh;
+  justify-content: center;
+`
+const WrapperSt = styled.div `
+  display: grid;
+  width: 90vw;
+  grid-template-columns: auto 20%;
+  align-items: center;
+`
+
+const A = styled.a `
+  align-self: center;
+  text-decoration: none;
+  color: white;
+  font-family: 'Squada One', cursive;
+`
+
+const Links = styled.div `
+  display: grid;
+  text-align: right;
+  grid-template-columns: repeat(4, auto);
+  text-align: right;
+`
 const Header = props => {
   return (
     <>
-        <header>
-          <div className="wrapper">
-            <Link href="/">
-              <a className="logo">
-                <h1 className="logo">Geek News</h1>
-              </a>
+      <HeaderSt>
+        <WrapperSt>
+          <Link href="/">
+            <A className="logo">
+              <h1 className="logo">Geek News</h1>
+            </A>
+          </Link>
+          <Links className="links">
+            <Link href="#">
+              <A>Новости</A>
             </Link>
-            <div className="links">
-              <Link href="#">
-                <a>Новости</a>
-              </Link>
-              <Link href="#">
-                <a>Личный кабинет</a>
-              </Link>
-              <Link href="#">
-                <a>Вход</a>
-              </Link>
-            </div>
-          </div>
-        </header>
+            <Link href="#">
+              <A>Личный кабинет</A>
+            </Link>
+            <Link href="#">
+              <A>Вход</A>
+            </Link>
+          </Links>
+        </WrapperSt>
+      </HeaderSt>
     </>
-  );
-};
+  )
+}
 
 export default Header;
