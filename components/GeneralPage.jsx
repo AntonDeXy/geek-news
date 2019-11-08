6cc4776cda14d44d47b9b2f1c5057c7194d6d4b8
AntonDeXy <porolyananton@gmail.com>
2019-11-08 15:34:42 +0200

    change axios request in main page

--- components/GeneralPage.jsx
+++ components/GeneralPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react"
 import TopArticles from "./GeneralPage/TopArticles"
 import MoreNews from './GeneralPage/MoreNews'
-import axios from 'axios'
 import Loader from './common/Loader';
 import { MainGeneralPageSt, TopArticlesBlockSt } from './elements/Main-styled';
+import { get } from '../static/functions';
 
 const GeneralPage = () => {
   const [articles, setArticles] = useState(null)
@@ -12,10 +12,7 @@ const GeneralPage = () => {
   ];
 
   useEffect(() => {
-    (async () => {
-      const res = await axios.get('https://cors-anywhere.herokuapp.com/geek-news-backend.herokuapp.com/articles')
-      setArticles(res.data)
-    })()
+    get('/articles', '', (res) => {setArticles(res)})
   }, [])
 
   return (
